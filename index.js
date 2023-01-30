1240e92e5c04302e886f6d2c2db8bb358ebffd5d
CMH5701 <skdaks5701@naver.com>
2023-01-30 19:07:07 +0900

    이름 입력시 사용중인 프레임워크 출력 API

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+//express
 const express = require('express')
 const app = express()
-app.listen(8080)
+//
 
-var cors =require('cors')//cors 에러 방지 npm i cors
+//포트번호 아무거나 가능 ex)1234,3000,8000 등등 통상적?으로 3000이나 8000,8080 하자
+app.listen(8080)
+//
+
+//cors
+var cors =require('cors')//cors 에러 방지 npm i cors or npm install cors 둘 다 가능s django로 할때 겪은적 있음
 app.use(cors ())
+//
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/test.html')//기다림 res send는 변수명 아님 바꾸면 오류
+  res.sendFile(__dirname + '/test.html')//res.send는 변수명 아님 바꾸면 오류 페이지 연결시 dirname도 해줘야함 없으면 연결x
 })
 
 app.get('/read', function (req, res) {
-    res.send("<a href = 'https://www.youtube.com/watch?v=Tt_tKhhhJqY'>read페이지</a>")//send 원하는 데이터
+    res.send("<a href = 'https://nodejs.org/en/'>노드 페이지</a>")//send 원하는 데이터
   })
+
 app.get('/write', function (req, res) {
     res.json({'sound':'야옹'})//send = json 동일하게 값 넘어감 json 사용시 명시적으로 사용하기 좋음
   })
@@ -24,8 +32,9 @@ app.get('/user/:id', function (req, res) {
     console.log(q.id)
     console.log(q.name)
     console.log(q.age)
+
   //http://localhost:8080/user/cmh?q=1&name=cmh localhost= 내가 정한 포트주소 '/'라우팅 주소 /id = cmh 로 저장 
-    //쿼리문은 ex)user/id/testcmh?q=임의 값 & name = 임의 값
+  //쿼리문은 ex)user/id/testcmh?q=임의 값 & name = 임의 값
 
     res.json({'이름뭐야?': q.name}) //주소창에 입력한 값 출력 id = *** name = mh = {"이름뭐야?" : "cmh"} 출력
 
