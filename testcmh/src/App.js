0aa0292a2b3d2dabfa494c736d71537b74683a72
CMH5701 <skdaks5701@naver.com>
2023-02-04 20:33:48 +0900

    react 연습용

---
+++ testcmh/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          리엑트는 바로바로 바뀐다잉~ 므찌다...
+          <br></br>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
